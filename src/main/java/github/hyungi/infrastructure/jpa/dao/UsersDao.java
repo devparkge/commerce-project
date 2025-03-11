41dbf57f6a882cd140b4733f9b2814b00470a4f5
PARKGE <phg5459@naver.com>
2025-03-11 23:07:44 +0900

    findByEmail 메소드 추가

--- src/main/java/github/hyungi/infrastructure/jpa/dao/UsersDao.java
+++ src/main/java/github/hyungi/infrastructure/jpa/dao/UsersDao.java
@@ -3,7 +3,9 @@ package github.hyungi.infrastructure.jpa.dao;
 import github.hyungi.infrastructure.jpa.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsersDao extends JpaRepository<UsersEntity, UUID> {
+    Optional<UsersEntity> findByEmail(String email);
 }
