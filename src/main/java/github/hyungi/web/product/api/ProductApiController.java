d05484f624838bd643b2539fed3a69efc8e20960
PARKGE <phg5459@naver.com>
2025-03-14 23:33:07 +0900

    이미지 업로드 로직 수정

--- src/main/java/github/hyungi/web/product/api/ProductApiController.java
+++ src/main/java/github/hyungi/web/product/api/ProductApiController.java
@@ -7,10 +7,8 @@ import github.hyungi.web.product.model.request.AddProductRequest;
 import github.hyungi.web.product.model.response.ProductResponse;
 import github.hyungi.web.product.model.response.wrapper.ProductResponseWrapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -20,23 +18,29 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/products")
 public class ProductApiController {
-    private final String imageUploadDir;
+    private final Path uploadPath;
     private final AddProductService addProductService;
 
     public ProductApiController(@Value("${image.upload-dir}") String imageUploadDir, AddProductService addProductService) {
-        this.imageUploadDir = imageUploadDir;
+        this.uploadPath = Paths.get(imageUploadDir).toAbsolutePath().normalize();
         this.addProductService = addProductService;
+
+        if (!uploadPath.toFile().exists()) {
+            uploadPath.toFile().mkdirs();
+        }
     }
 
     @PutMapping("/product-add")
     public ProductResponseWrapper addProduct(
             @JwtAuthentication UUID userId,
-            @RequestBody AddProductRequest addProductRequest
+            @RequestPart("image") MultipartFile image,
+            @RequestPart("product") AddProductRequest addProductRequest
     ) throws IOException {
-        String fileName = UUID.randomUUID() + "_" + addProductRequest.image().getOriginalFilename();
-        Path imagePath = Paths.get(imageUploadDir).resolve(fileName);
 
-        addProductRequest.image().transferTo(imagePath.toFile());
+        String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+        Path imageFilePath = uploadPath.resolve(fileName);
+
+        image.transferTo(imageFilePath.toFile()); // 파일 저장
 
         Product product = addProductService.addProduct(
                 userId,
